6a380ef7c796632e515215fd2a9de9c44b7d92ed
Thomas Ferré <t.ferre@digitsole.com>
2021-04-20 14:12:41 +0200

    Layout implementation-V1.1

---
+++ P04_Instagrid/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P04_Instagrid
+//
+//  Created by TomF on 14/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

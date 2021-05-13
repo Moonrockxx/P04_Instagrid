55537762311657b0912f8a8db8cd5812071dd947
Thomas Ferré <t.ferre@digitsole.com>
2021-05-13 12:13:46 +0200

    [Swipe Label] Label and Icon
    - Implement function who change label text on depend to orientation mode
    - Fix size of chevron left
    - Add Spacing on the swipe stack

--- P04_Instagrid/Controller/ViewController.swift
+++ P04_Instagrid/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // - Swipe Label
     @IBOutlet weak var swipeLabel: UILabel!
     
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func selectLayout1(_ sender: Any) {
         gridTopLeft.isHidden = true
         gridTopRight.isHidden = true
@@ -56,7 +56,6 @@ class ViewController: UIViewController {
         rectangleBottom.isHidden = false
     }
     
-    
     @IBAction func selectLayout3(_ sender: Any) {
         gridTopLeft.isHidden = false
         gridTopRight.isHidden = false
@@ -65,27 +64,12 @@ class ViewController: UIViewController {
         gridBottomRight.isHidden = false
         rectangleBottom.isHidden = true
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.current.orientation.isLandscape {
+            self.swipeLabel.text = "Swipe left to share"
+        } else {
+            self.swipeLabel.text = "Swipe up to share"
+        }
+    }
 }
-
-//let view1 = UIView()
-//let view2 = UIView()
-//let view3 = UIView()
-//
-//@IBAction func showView1BtnPressed() {
-//    hideOtherViewsExcept(forView: view1)
-//}
-//
-//@IBAction func showView2BtnPressed() {
-//    hideOtherViewsExcept(forView: view2)
-//}
-//
-//@IBAction func showView3BtnPressed() {
-//    hideOtherViewsExcept(forView: view3)
-//}
-//
-//func hideOtherViewsExcept(forView view: UIView) {
-//    view1.isHidden = true
-//    view2.isHidden = true
-//    view3.isHidden = true
-//    view.isHidden = false
-//}
